28f6e2755dffdcbf21f53e8691b7469847c3783d
MattiaPaiano91 <paianomattia1@gmail.com>
2024-03-13 16:38:29 +0100

    Completata Navigazione nelle pagine

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -47,6 +47,23 @@ export default {
             console.error('Errore nella chiamata API:', error);
             });
             }
+        },
+        getPage(i){
+               axios
+            .get('http://localhost:8000/api/projects',{
+                params:{
+                    page: i
+                }
+            })
+            .then(response => {
+            console.log(response.data);
+            this.projects = response.data.result.data;
+            this.currentPage = response.data.result.current_page;
+            this.lastPage = response.data.result.last_page;
+            })
+            .catch(error => {
+            console.error('Errore nella chiamata API:', error);
+            });
         }
     },
     components:{
@@ -75,21 +92,31 @@ export default {
 
 <template>
     <main>
-        <div class="container-fluid d-flex ">
+        <div class="container-fluid d-flex justify-content-center ">
                 <ProjectCard
                 v-for="project in projects"
                 :project="project"
                 />
         </div>
-        <button @click="prevPage()">
-            Pagina precedente
-        </button>
-        <button @click="nextPage()">
-            Pagina successiva
-        </button>
+        <div class="button-box d-flex justify-content-center mt-5">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li @click="prevPage()" class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li @click="getPage(i)" v-for="(page, i) in lastPage" class="page-item"><a class="page-link" href="#">{{ page }}</a></li>
+                    <li  @click="nextPage()" class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>
+        </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/main.scss" as *;
+main{
+    width: 100%;
+    .button-box{
+        width: 50%;
+        margin: 0 auto;
+    }
+}
 </style>
